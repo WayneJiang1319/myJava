6324f02ffe3aaf7c88c4c729cc2e85f0dff3176f
jiangw1024 <jiangw1024@163.com>
2018-11-25 11:04:46 +0800

    core java 1 ch3

---
+++ java_project/wayne-core-java1/src/chapter3/FirstSample.java
@@ -0,0 +1,13 @@
+package chapter3;
+//Java need to import 
+public class FirstSample {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World,Welcome to Core Java I Edition 9 !");
+
+	}
+
+}
