46b75081be772fc9334ece92b3e3335a505c4397
jiangw1024 <jiangw1024@163.com>
2018-11-25 08:28:55 +0800

    add 20181125

--- java_project/20181124/waye-code-java/src/FirstHelloWorld.java
+++ java_project/20181124/waye-code-java/src/FirstHelloWorld.java
@@ -4,6 +4,7 @@ public class FirstHelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World !");
+		System.out.println("Today is 2018/11/25");
 
 	}
 
